a5637161e7a2ffd3ea037060547c6ce6413c4ada
rituarora2013 <mailtorituarora@yahoo.com>
2017-05-15 14:41:52 +0530

    Added constructors

--- SnakesAndLadders/src/gameBoard/Ladder.java
+++ SnakesAndLadders/src/gameBoard/Ladder.java
@@ -1,5 +1,17 @@
 package gameBoard;
 
 public class Ladder {
+	
+	int startPosition;
+	int endPosition;
+	int colorElement;
+	
+	Ladder(int start, int end, int color) 
+	{
+		startPosition= start;
+		endPosition= end;
+		colorElement= color;
+	}
+
 
 }
--- SnakesAndLadders/src/gameBoard/Snake.java
+++ SnakesAndLadders/src/gameBoard/Snake.java
@@ -2,4 +2,15 @@ package gameBoard;
 
 public class Snake {
 
+	int startPosition;
+	int endPosition;
+	int colorElement;
+	
+	Snake(int start, int end, int color) 
+	{
+		startPosition= start;
+		endPosition= end;
+		colorElement= color;
+	}
+
 }
